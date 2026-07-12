b00a2cfd7716b8fa184c9827d5bfb0bf69c5b574
Guzmán Fajo <guzmanfajo@gmail.com>
2026-07-11 21:55:12 -0300

    chore: configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
+const redirect = sessionStorage.redirect;
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+
+  window.history.replaceState(
+    null,
+    "",
+    import.meta.env.BASE_URL + redirect
+  );
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
