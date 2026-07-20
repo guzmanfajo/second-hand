6c99c496621a468eefd8a8b76c175ff9c32f76e0
Guzmán Fajo <guzmanfajo@gmail.com>
2026-07-20 14:53:08 -0300

    chore: prepare project for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,9 @@ import App from './App.jsx';
 import { CartProvider } from './context/CartContext';
 import { ToastProvider } from './context/ToastContext';
 
-
-const redirect = sessionStorage.redirect;
-
-if (redirect) {
-  sessionStorage.removeItem("redirect");
-
-  window.history.replaceState(
-    null,
-    "",
-    import.meta.env.BASE_URL + redirect
-  );
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <ToastProvider>
         <CartProvider>
           <App />
